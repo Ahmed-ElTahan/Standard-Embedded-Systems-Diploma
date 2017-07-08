980516ea212189be741f42800b8b25853791f947
Ahmed Eltahan <ahmed.eltahan94@eng-st.cu.edu.eg>
2017-07-08 22:37:25 +0200

    Add files via upload

---
+++ Learning C/C codes/7- Addition/Addition.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main()
+{
+// declaration
+	int no_1;
+	int no_2; 
+	int sum;
+	
+// get the values from the user
+	printf("Please enter no.1: ");
+	scanf("%d", &no_1);
+	printf("Please enter no.2: ");
+	scanf("%d", &no_2);
+
+// Summation operation
+	sum = no_1 + no_2;
+	printf("Sum equal to %d", sum);
+
+
+}
