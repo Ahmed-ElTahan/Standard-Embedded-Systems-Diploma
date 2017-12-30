8e3d460f369b41977c653b04a69a468aad5ec110
Ahmed-ElTahan <ahmed.eltahan94@eng-st.cu.edu.eg>
2017-12-30 13:13:18 +0200

    delete old files

--- ATMega32 Project Files/48_ٍSPI_App1_Slave/Debug/DDIO.d
+++
@@ -1,7 +0,0 @@
-DDIO.o DDIO.o: ../DDIO.c ../DDIO.h ../STD_TYPES.h ../UTILS.h
-
-../DDIO.h:
-
-../STD_TYPES.h:
-
-../UTILS.h:
--- ATMega32 Project Files/48_ٍSPI_App1_Slave/Debug/main.d
+++
@@ -1,7 +0,0 @@
-main.o main.o: ../main.c ../DDIO.h ../STD_TYPES.h ../UTILS.h
-
-../DDIO.h:
-
-../STD_TYPES.h:
-
-../UTILS.h:
